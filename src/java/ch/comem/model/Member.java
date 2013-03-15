484a437a59609c42e2da4e4dbb5350f0c088008d
WAYOFFprod <c0rkn0t@gmail.com>
2013-03-15 14:54:43 +0100

    update

--- src/java/ch/comem/model/Member.java
+++ src/java/ch/comem/model/Member.java
@@ -20,7 +20,32 @@ public class Member implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String name;
+    private String firstName;
+    private String lastName;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
     private int age;
 
     public Long getId() {
