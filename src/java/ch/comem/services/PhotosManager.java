4b1ae4d26b1abcd54ecb10d364a14ab0d95df2ac
WAYOFFprod <c0rkn0t@gmail.com>
2013-03-16 10:05:20 +0100

    update

---
+++ src/java/ch/comem/services/PhotosManager.java
@@ -0,0 +1,35 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ch.comem.services;
+
+import ch.comem.model.Photo;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author raphaelbaumann
+ */
+@Stateless
+public class PhotosManager implements PhotosManagerLocal {
+    @PersistenceContext(unitName = "PastyChefPU")
+    private EntityManager em;
+    
+    @Override
+    public Long createPhoto(String source, String alt) {
+        Photo photo = new Photo();
+        photo.setSource(source);
+        photo.setAlt(alt);
+        return photo.getId();
+    }
+
+    public void persist(Object object) {
+        em.persist(object);
+    }
+
+    
+
+}
---
+++ src/java/ch/comem/services/PhotosManagerLocal.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ch.comem.services;
+
+import javax.ejb.Local;
+
+/**
+ *
+ * @author raphaelbaumann
+ */
+@Local
+public interface PhotosManagerLocal {
+
+    Long createPhoto(String source, String alt);
+    
+}
